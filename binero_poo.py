8113053d8fa0a3ac2452096e7313e640e1368421
jux160 <julien.gadotti1@gmail.com>
2023-01-06 07:41:28 +0100

    nv

--- binero_poo.py
+++ binero_poo.py
@@ -1,5 +1,4 @@
-class binero():
-    
+class binero:   
     def __init__(self,difficulte=1):
         """methode constructor qui defini le plateau de jeux et le plateau de correction en fonction duint renseigner 
 
@@ -10,65 +9,55 @@ class binero():
         """
         
         if difficulte == 1: 
-            self.plateau =  [["*","*","*","*","*",1], #grille de depart du niveaux 1
-                             ["*",0,"*","*",0,"*",],
-                             ["*","*","*","*","*",1],
-                             ["*",0,1,"*",1,"*",],
-                             ["*","*","*",0,"*","*",],
-                             [0,"*",1,"*","*","*",]]
-        
-            self.plateau_correcte = [["0","1","0","0","1",1], #grille de correction du niveaux 1
-                                     ["1",0,"1","1",0,"0"],
-                                     ["0","1","0","1","0",1],
-                                     ["1",0,1,"0",1,"0"],
-                                     ["1","1","0",0,"1","0"],
-                                     [0,"0",1,"1","0","1"]]
+            # generation du plateu de jeux et du plateau de corections
+            self.plateau = self.convertion("niveaux/simple.txt")[0]
+            self.plateau_correct = self.convertion("niveaux/simple.txt")[1]
+            
         elif difficulte == 2:
-            self.plateau = [[1,1,"*","*","*",1,"*","*","*","*"], #grille de depart du niveaux 2
-                            ["*","*","*","*","*","*","*","*",0,"*"],
-                            [0,1,"*",0,"*","*",1,"*","*","*"],
-                            [1,"*","*",0,"*","*","*","*","*","*"],
-                            ["*","*",1,"*","*","*",0,"*",1,1],
-                            ["*","*","*","*","*","*",0,"*","*","*"],
-                            [0,"*","*","*","*","*","*","*","*",0],
-                            ["*","*",1,"*","*",0,"*","*","*","*"],
-                            ["*","*",1,1,"*","*",1,0,"*",0],
-                            ["*",0,"*","*","*","*","*","*","*",0]]
-
-            self.plateau_correcte = [[1,1,"0","0","1",1,"0","1","0","0"],
-                                    ["0","0","1","1","0","1","0","1",0,"1"],  #grille de correction du niveaux 2
-                                    [0,1,"0",0,"1","0",1,"0","1","1"],
-                                    [1,"1","0",0,"1","0","1","1","0","0"],
-                                    ["0","0",1,"1","0","1",0,"0",1,1],
-                                    ["1","0","1","0","1","0",0,"1","0","1"],
-                                    [0,"1","0","1","0","1","1","0","1",0],
-                                    ["0","1",1,"0","1",0,"0","1","0","1"],
-                                    ["1","0",1,1,"0","0",1,0,"1",0],
-                                    ["1",0,"0","1","0","1","1","0","1",0]]
-
+            # on repete la meme chose pour le niveaux 2
+            self.plateau = self.convertion("niveaux/moyen.txt")[0]
+            self.plateau_correct = self.convertion("niveaux/moyen.txt")[1]
         elif difficulte == 3:
-            self.plateau = [["*",0,"*","*",1,1,"*","*","*",1],
-                            ["*","*",0,"*","*","*","*","*",0,"*"],
-                            ["*",1,"*",1,"*","*","*","*","*","*"],
-                            ["*",1,"*","*",0,"*","*","*","*","*"], #grille de depart du niveaux 3
-                            ["*","*","*","*","*","*","*",0,1,"*"],
-                            [1,"*","*",0,"*","*","*",0,"*","*"],
-                            [1,1,"*","*","*","*","*","*","*","*"],
-                            ["*","*","*","*","*","*","*","*","*",0],
-                            ["*","*","*",1,"*","*","*",1,"*","*"],
-                            ["*","*",0,"*","*","*","*","*",1,"*"]]
+            self.plateau = self.convertion("niveaux/difficile.txt")[0]
+            self.plateau_correct = self.convertion("niveaux/difficile.txt")[1]
+    def convertion(self, fichier):
 
-            self.plateau_correcte = [["0",0,"1","0",1,1,"0","0","1",1],
-                                    ["1","0",0,"1","0","1","1","0",0,"1"],
-                                    ["1",1,"0",1,"1","0","0","1","0","0"],
-                                    ["0",1,"1","0",0,"1","0","1","1","0"],
-                                    ["0","0","1","1","0","0","1",0,1,"1"],  #grille de correction du niveaux 3
-                                    ["1","1","0",0,"1","0","1",0,"0","1"],
-                                    [1,1,"0","0","1","1","0","1","0","0"],
-                                    ["0","0","1","1","0","1","1","0","1",0],
-                                    ["0","0","1",1,"0","0","1",1,"0","1"],
-                                    ["1","1",0,"0","1","0","0","1",1,"0"]]
-    
+        """methode qui convertie le fichier du niveaux en liste de liste 
+
+        Args:
+            fichier (str): fichier du niveaux
+        
+        return:
+            liste avec la grille en occurence 1 et la correctioon en 2
+        """
+        f = open(fichier, "r") #overture du fichier du niveaux 1
+        taille = int(f.readline()) #lecture de la premier ligne qqui definie le nombre de ligne du plateau 
+        grille = [] #definitin de la grille de jeux qui par la suite sera ensuite l'attribut plateau
+        correction = [] #grille de corrections
+        for i in range(taille):# boucle qui permet de lire les ligne du plateaux de jeux 
+            line = f.readline() #on lis la ligne et on la stock ensuite dans la variable line
+            line = line.replace("\n", "").replace(" ", "*") #on remplace les espace par des *
+            grille.append(list(line))#on ajoute la ligne a grille sous forme de liste de strings
+        #permet de convertir les 0 et 1 qui sont en str en int pour les differencier entre les 0,1 ajouter par l,utilisateur et ceux deja present sur le plateau
+        for ligne in range(len(grille)):
+            for car in range(len(grille[i])):
+                if grille[ligne][car] == "1":
+                    grille[ligne][car] = 1
+                elif grille[ligne][car] == "0":
+                    grille[ligne][ligne] = 0            
+        #definition du plateau de jeux de correction
+        for ligne in range(taille):
+            line = f.readline()
+            line = line.replace("\n", "")
+            correction.append(list(line))
+        for ligne in range(len(grille)):
+            for car in range(len(grille)):
+                if grille[ligne][car] == "2":#on remplce les 2 par des 1 et les trois par des 0
+                    grille[ligne][car] = 1
+                elif grille[ligne][car] == "3":
+                    grille[ligne][car] = 0
+        f.close()
+        return [grille,correction]  
     def entre_modif(self):
         """methode permetant de demander aux joueur quelle element il veux modifier et de les modifier
         """
@@ -100,8 +89,6 @@ class binero():
 
         #on modifie par le nombre corespondant en str pour pouvoir le remodifier par la suite si besoin
         self.plateau[ligne-1][colonne-1] = car  # type: ignore
-
-
     def entre(self):
         """fonction qui demande a l'utilisateur de rentrer une ligne ou un colonne et verifie si la saisie est bonne, 
            si cela ne sort pas de l'index du plateau
@@ -128,19 +115,19 @@ class binero():
                 print("votre saisie est incorrect veuillez resaissir avec un chiffre correct ")
                 entre = (input(": ")) #ligne repasse en str pour pouvoir recommencer le try except pour verifier la saisie fait est bonne 
 
-        return entre
+        return entre 
+    def __str__(self):
+        """Renvoie de l'utilisateur
+        return la chaine du plateau"""
+        chaine = ""
 
-    def affichage(self):
-        """
-        affiche le plateau de jeux 
-        """
         for i in self.plateau:
             for k in i:
-                print(k, end=" ")
-            print()
+                chaine += str(k) + " "
 
-
+            chaine += "\n"
     
+        return chaine
     def correction(self):
         """methode qui verifie si le plateau du joueur est correcte 
 
@@ -176,48 +163,52 @@ class binero():
             choix = int(input("veuillez ressaisir: ")) # demande a l'utilisateur de ressaisir si sa premier saisie est fausse 
         return choix 
 
-jeux = True        
+a = binero(3)
+print(a.__str__())
+"""
+if __name__ == "__main__":
 
-while jeux:
+    jeux = True
+    while jeux:
 
-    choix = binero.menu()
+        choix = binero.menu()
 
-        #conditions pour savoir se que l'utilisateur a saisie et le renvoyer vers le bon chemin 
-    if choix == 1:
-        #print les regle du jeux 
-        print("\n" + "dans se jeux il y a que 3 régle :" + "\n" + 
-            "-il faut qu'il y est autant de 0 que de 1 sur une meme ligne et sur une meme colonne" + "\n" + 
-            "-il ne peux pas y avoir plus de deux 0 ou deux 1 a la suite en horizontale et en verticale" + "\n" +                 
-            "- il ne peux pas avoir deux ligne ou colonne identique"  + "\n")
-        input("entrer" + "\n") # attend que l'utilisateur tape une touche pour continuer
-        
-    elif choix == 2:
-        #print le but du jeux 
-        print("\n" + "le but du jeux est de completer entierement la grille sans faire d'erreur" + "\n")
-        input("entrer" + "\n") # attend que l'utilisateur tape une touche pour continuer
-                
-    elif choix == 3:
-        #renvoie le joueur vers le niveau 1
-        grille = binero(1)
-        grille.jeux()        
-        
-    elif choix == 4:
-        #renvoie le joueur vers le niveau 2
-        grille= binero(2)
-        grille.jeux()   
+            #conditions pour savoir se que l'utilisateur a saisie et le renvoyer vers le bon chemin 
+        if choix == 1:
+            #print les regle du jeux 
+            print("\n" + "dans se jeux il y a que 3 régle :" + "\n" + 
+                "-il faut qu'il y est autant de 0 que de 1 sur une meme ligne et sur une meme colonne" + "\n" + 
+                "-il ne peux pas y avoir plus de deux 0 ou deux 1 a la suite en horizontale et en verticale" + "\n" +                 
+                "- il ne peux pas avoir deux ligne ou colonne identique"  + "\n")
+            input("entrer" + "\n") # attend que l'utilisateur tape une touche pour continuer
+            
+        elif choix == 2:
+            #print le but du jeux 
+            print("\n" + "le but du jeux est de completer entierement la grille sans faire d'erreur" + "\n")
+            input("entrer" + "\n") # attend que l'utilisateur tape une touche pour continuer
+                    
+        elif choix == 3:
+            #renvoie le joueur vers le niveau 1
+            grille = binero(1)
+            grille.jeux()        
+            
+        elif choix == 4:
+            #renvoie le joueur vers le niveau 2
+            grille= binero(2)
+            grille.jeux()   
 
-    elif choix == 5:
-        #renvoie le joueur vers le niveau 3
-        grille= binero(3)
-        grille.jeux()   
+        elif choix == 5:
+            #renvoie le joueur vers le niveau 3
+            grille= binero(3)
+            grille.jeux()   
 
-    #conditions pour savoir si le joueur etait en train de jouer ou pas, si il jouait alors on lui demande si il veux rejouer 
-    if choix == 3 or choix == 4 or choix == 5:
-        continuer = input("voulez vous rejouer ou pas (oui/ non : ")
-        while continuer != "oui" and continuer != "non":
-            continuer = input("veuillez ressaisir avec un oui ou un non :")
+        #conditions pour savoir si le joueur etait en train de jouer ou pas, si il jouait alors on lui demande si il veux rejouer 
+        if choix == 3 or choix == 4 or choix == 5:
+            continuer = input("voulez vous rejouer ou pas (oui/ non : ")
+            while continuer != "oui" and continuer != "non":
+                continuer = input("veuillez ressaisir avec un oui ou un non :")
 
-        #conditions qui permet d'arreter la boucle principale en passant jeux a False 
-        if continuer == "non":
-            jeux == False
-         
+            #conditions qui permet d'arreter la boucle principale en passant jeux a False 
+            if continuer == "non":
+                jeux == False
+            """
